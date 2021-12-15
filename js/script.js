cec0259e2e42036fd79263f7d5eaf442edd75bf8
AMinding <a.minding@web.de>
2021-12-15 15:24:06 +0100

    Mit Kommentaren versehen

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 
 let solution = [];
 let solutionProgress = [];
-let falseLetters = [];
 let lifes = 13;
 let gameOver = false;
 let gameWon = false;
 let jsonData = {};
 
 let container = document.querySelector("#container");
+
+// ---------------------------------------------------------------
+// ----------- Funktion zum Laden des JSON-Scripts ---------------
+// ---------------------------------------------------------------
 
 function loadJsonData() {
     // 1. XHR-Objekt erzeugen
@@ -37,6 +40,13 @@ function loadJsonData() {
     xhr.send();
 }
 
+
+// ---------------------------------------------------------------
+// -------- Funktionen zum Spielschema/den Spielregeln -----------
+// ---------------------------------------------------------------
+
+
+// Gesuchtes Wort wird aus JSON-Datei ausgewählt
 function chooseRandomWord(jsonData) {
     let numberOfWords = jsonData.words.length;
     let randomNumber = Math.round(Math.random() * (numberOfWords - 1));
@@ -47,10 +57,10 @@ function chooseRandomWord(jsonData) {
 }
 
 
+// Ausgangslage für jeden Rundenbeginn + Umwandlung des gesuchten Wortes in Unterstriche
 function newGame (word) {
     solution = [];
     solutionProgress = [];
-    falseLetters = [];
     lifes = 13;
     gameOver = false;
     gameWon = false;
@@ -63,6 +73,7 @@ function newGame (word) {
 }
 
 
+// Funktion zur Reduktion verbliebener Leben + Definition der Bedingung für GameOver
 function reduceLifesIfNotFound (letterFound) {
     if (letterFound === false) {
         lifes--;
@@ -73,6 +84,7 @@ function reduceLifesIfNotFound (letterFound) {
 }
 
 
+// Definition der Bedingung für GameWon
 function checkIfPlayerHasWon () {
     let allEqual = true;
     for (let i = 0; i < solution.length; i++) {
@@ -87,6 +99,7 @@ function checkIfPlayerHasWon () {
 }
 
 
+// Abgleich, ob geratener Buchstabe in Lösungswort vorhanden und Einsetzen in solutionProgress
 function checkIfLetterIsInSolution (letter) {
     let letterFound = false;
     for (let i = 0; i < solution.length; i++) {
@@ -100,23 +113,26 @@ function checkIfLetterIsInSolution (letter) {
 }
 
 
+// Aufruf der vorherigen drei Funktionen 
 function guessLetter (letter) {
     let letterFound = checkIfLetterIsInSolution(letter.toUpperCase());
     reduceLifesIfNotFound(letterFound);
     checkIfPlayerHasWon();
-
-
-    if (letterFound === false) {
-        falseLetters.push(letter);
-    }
 }
 
+
+
+// ---------------------------------------------------------------
+// ------ Funktionen zur Darstellung des Spiels im Browser -------
+// ---------------------------------------------------------------
+
+// click-Eventlistener zum Startbutton hinzufügen und Seiteninhalte ein- und ausblenden
 window.addEventListener("DOMContentLoaded", function () {
     let startButton = document.querySelector("#nameSubmit");
     if (startButton) {
         startButton.addEventListener("click", changeContent);
     }
-
+// Erzeugung Buchstaben-Buttons im DOM sowie dazugehörige Click-Events
     let abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     for (let letter of abc) {
         let letterButton = document.createElement("button");
@@ -127,17 +143,19 @@ window.addEventListener("DOMContentLoaded", function () {
 
         document.querySelector(".letters").appendChild(letterButton);
     }
-
+// Funktionsaufruf der JSON-Datei
     loadJsonData();
+// aktueller Highscore wird geladen
     showHighscore();
 });
 
+// Funktionsaufrufe zum Start des neuen Spiels + Einblenden der Wort-Striche inkl. Leerzeichen
 function startGame() {
     let randomWord = chooseRandomWord(jsonData);
     newGame(randomWord);
     updateMaskedSolution();
     
-
+// Anzeige der verbleibenden Leben + Ausblenden der geklickten Buchstaben-Buttons
     document.querySelector("#trysLeft").innerText = "Leben: " + lifes;
     let letterButtons = document.getElementsByClassName("letterButton");
     for (let letterButton of letterButtons) {
@@ -145,11 +163,14 @@ function startGame() {
     }
 }
 
+// Einblenden der Wort-Striche inkl. Leerzeichen
 function updateMaskedSolution() {
     let maskedSolution = solutionProgress.join(" ");
     document.getElementById("maskedSolution").innerText = maskedSolution;
 }
 
+
+// Funktion zum Wechsel des angezeigten Inhalts nach Spielstart
 function changeContent() {
     if (document.querySelector("#nameBox").value.trim().length == 0) {
         alert("Ungültiger Name");
@@ -162,6 +183,8 @@ function changeContent() {
     classInvisible.classList.add("gameInvisible");
 }
 
+
+// Erzeugung eines Highscores mit LocalWebStorage
 function highscore() {
     let playerName = document.querySelector("#nameBox").value;
     let highscoreData = JSON.parse(localStorage.getItem("highscore"));
@@ -180,6 +203,8 @@ function highscore() {
     }
 }
 
+
+// Übertragung der geklickten Buchstaben auf Anzeige des geratenen Wortes + Funktionsaufruf Übrprüfung Spielende
 function clickLetter(event) {
     let clickedLetterElement = event.srcElement;
     let clickedLetter = clickedLetterElement.innerText;
@@ -192,6 +217,9 @@ function clickLetter(event) {
     gameEnd();
 }
 
+
+// Funktion zur Überprüfung der Bedingungen für sämtliche Spielenden 
+// + Funktionsaufruf zur Anzeige des aktuellen Highscores + Einbledung des Startbildschirms
 function gameEnd () {
     if (gameOver === true || gameWon === true) {
         let classVisible = document.getElementById("gameStartVisible");
@@ -212,6 +240,7 @@ function gameEnd () {
     showHighscore();
 }
 
+// Funktion zur Anzeige des aktuellen Highscores
 function showHighscore () {
     let highscoreData = JSON.parse(localStorage.getItem("highscore"));
     if (highscoreData != null) {
